ad387328fe645b36b551eacfc4ee6c235ee13673
Афанасьев Виталий <17476018U@MacBook-Pro-internet-5.local>
2019-12-28 19:26:52 +0300

    add cors, cors for all

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 import asyncio
 from aiohttp import web
 from aiohttp_swagger import setup_swagger
+import aiohttp_cors
 import people
 import appointments
 
@@ -14,5 +15,16 @@ app.router.add_route("GET", "/people/appointments", appointments.read)
 app.router.add_route("POST", "/people/appointments", appointments.create)
 app.router.add_route("PUT", "/people/appointments/{appointment_id}", appointments.update)
 app.router.add_route("DELETE", "/people/appointments/{appointment_id}", appointments.delete)
+
+cors = aiohttp_cors.setup(app, defaults={
+    "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+})
+
+for route in list(app.router.routes()):
+    cors.add(route)
 
 setup_swagger(app, swagger_url="/ui", ui_version=2, swagger_from_file="swagger.yaml")
